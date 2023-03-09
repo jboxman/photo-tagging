3c6c6608696ae9c39953bb09e560e4d85dd0de71
Jason Boxman <jasonb@edseek.com>
2023-03-09 00:02:41 -0500

    Checkpoint

--- libs/ui/src/TreeView.stories.jsx
+++ libs/ui/src/TreeView.stories.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import MinusIcon from '@patternfly/react-icons/dist/esm/icons/minus-icon';
@@ -15,150 +16,64 @@ import {
   EmptyStateBody,
   Bullseye,
   Button,
+  Title,
+  Form,
+  FormGroup,
+  TextInput,
+  ActionGroup
 } from '@patternfly/react-core';
 
-const data = [
-  {
-    name: 'Application launcher',
-    id: 'example3-AppLaunch',
-    checkProps: { 'aria-label': 'app-launcher-check', checked: false },
-    children: [
-      {
-        name: 'Application 1',
-        id: 'example3-App1',
-        checkProps: { checked: false },
-        children: [
-          {
-            name: 'Settings',
-            id: 'example3-App1Settings',
-            checkProps: { checked: false },
-          },
-          {
-            name: 'Current',
-            id: 'example3-App1Current',
-            checkProps: { checked: false },
-          },
-        ],
-      },
-      {
-        name: 'Application 2',
-        id: 'example3-App2',
-        checkProps: { checked: false },
-        children: [
-          {
-            name: 'Settings',
-            id: 'example3-App2Settings',
-            checkProps: { checked: false },
-          },
-          {
-            name: 'Loader',
-            id: 'example3-App2Loader',
-            checkProps: { checked: false },
-            children: [
-              {
-                name: 'Loading App 1',
-                id: 'example3-LoadApp1',
-                checkProps: { checked: false },
-              },
-              {
-                name: 'Loading App 2',
-                id: 'example3-LoadApp2',
-                checkProps: { checked: false },
-              },
-              {
-                name: 'Loading App 3',
-                id: 'example3-LoadApp3',
-                checkProps: { checked: false },
-              },
-            ],
-          },
-        ],
-      },
-    ],
-    defaultExpanded: true,
-  },
-  {
-    name: 'Cost management',
-    id: 'example3-Cost',
-    checkProps: { 'aria-label': 'cost-check', checked: false },
-    children: [
-      {
-        name: 'Application 3',
-        id: 'example3-App3',
-        checkProps: { 'aria-label': 'app-3-check', checked: false },
-        children: [
-          {
-            name: 'Settings',
-            id: 'example3-App3Settings',
-            checkProps: {
-              'aria-label': 'app-3-settings-check',
-              checked: false,
-            },
-          },
-          {
-            name: 'Current',
-            id: 'example3-App3Current',
-            checkProps: {
-              'aria-label': 'app-3-current-check',
-              checked: false,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: 'Sources',
-    id: 'example3-Sources',
-    checkProps: { 'aria-label': 'sources-check', checked: false },
-    children: [
-      {
-        name: 'Application 4',
-        id: 'example3-App4',
-        checkProps: { 'aria-label': 'app-4-check', checked: false },
-        children: [
-          {
-            name: 'Settings',
-            id: 'example3-App4Settings',
-            checkProps: {
-              'aria-label': 'app-4-settings-check',
-              checked: false,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: 'Really really really long folder name that overflows the container it is in',
-    id: 'example3-Long',
-    checkProps: { 'aria-label': 'long-check', checked: false },
-    children: [
-      {
-        name: 'Application 5',
-        id: 'example3-App5',
-        checkProps: { 'aria-label': 'app-5-check', checked: false },
-      },
-    ],
-  },
-];
+const MyTreeView = () => {
+  const [data, setData] = useState(null);
+  const [activeItems, setActiveItems] = useState([]);
 
-const MyTreeView = () => (
-  <>
-    <TreeView
-      data={data}
-      defaultAllExpanded
-      hasGuides
-      onSelect={(e, item, parentItem) => {
-        console.log(e);
-      }}
-    />
-  </>
-);
+  useEffect(function () {
+    (async () => {
+      // https://dev.to/thatamymac/dynamic-imports-of-json-ipl
+      const data = await import('./fixtures/tag-tree.json').then(
+        (module) => module.default
+      );
+      setData(data);
+    })();
+  }, []);
+
+  return (
+    <>
+      {!data ? null : (
+        <TreeView
+          data={data}
+          defaultAllExpanded
+          hasGuides
+          hasSelectableNodes
+          activeItems={activeItems}
+          onSelect={(e, item, parentItem) => {
+            if (activeItems.includes(item)) setActiveItems([]);
+            if (!activeItems.includes(item)) setActiveItems([item]);
+          }}
+          className="smaller"
+        />
+      )}
+    </>
+  );
+};
+
+const TagForm = () => {
+  return (
+    <Form>
+      <FormGroup label="Tag" isRequired>
+        <TextInput isRequired type="text" />
+      </FormGroup>
+      <ActionGroup>
+        <Button variant="primary">Save</Button>
+        <Button variant="link">Cancel</Button>
+      </ActionGroup>
+    </Form>
+  );
+};
 
 export default {
-  title: 'MyTreeView',
-  component: MyTreeView,
+  title: 'Tag Manager',
+  component: MyTreeView
 };
 
 // fa-plus-circle
@@ -167,19 +82,21 @@ export default {
 
 const Template = (args) => {
   return (
-    <Bullseye className="smaller">
+    <>
       <Flex
         direction={{ default: 'row' }}
         spaceItems={{ default: 'spaceItemsNone' }}
         flexWrap={{ default: 'nowrap' }}
+        alignItems={{ default: 'alignItemsStretch' }}
       >
         <FlexItem>
+          <Title headingLevel="h1">h1 defaults to 2xl</Title>
           <Flex
             direction={{ default: 'column' }}
             spaceItems={{ default: 'spaceItemsNone' }}
             flexWrap={{ default: 'nowrap' }}
           >
-            <FlexItem>
+            <FlexItem className="scroll">
               <MyTreeView {...args} />
             </FlexItem>
             <FlexItem>
@@ -193,17 +110,22 @@ const Template = (args) => {
           </Flex>
         </FlexItem>
         <FlexItem>
-          <EmptyState>
-            <EmptyStateBody>This should be a form.</EmptyStateBody>
-          </EmptyState>
+          <TagForm />
         </FlexItem>
       </Flex>
-    </Bullseye>
+    </>
   );
 };
 
-export const Primary = Template.bind({});
+/*
+          <EmptyState>
+            <EmptyStateBody>This should be a form.</EmptyStateBody>
+          </EmptyState>
+*/
+
+export const Me = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
+Me.args = {
+  primary: true
 };
+Me.storyName = 'Default';
--- libs/ui/src/components/TagTree.jsx
+++ libs/ui/src/components/TagTree.jsx
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types';
 
 import { List, ListItem } from '@patternfly/react-core';
 
-import BookOpenIcon from '@patternfly/react-icons/dist/esm/icons/book-open-icon';
-import KeyIcon from '@patternfly/react-icons/dist/esm/icons/key-icon';
 import DesktopIcon from '@patternfly/react-icons/dist/esm/icons/desktop-icon';
 
 import '@patternfly/react-core/dist/styles/base.css';
@@ -28,7 +26,7 @@ const TagTree = () => {
   useEffect(function () {
     (async () => {
       // https://dev.to/thatamymac/dynamic-imports-of-json-ipl
-      const data = await import('./fixtures/tag-tree.json').then(
+      const data = await import('../fixtures/tag-tree.json').then(
         (module) => module.default
       );
       setData(data);
@@ -75,26 +73,4 @@ const TagTree = () => {
   return <>{data ? <List isPlain>{renderChildren(data)}</List> : null}</>;
 };
 
-/*
-    <List isPlain>
-      <ListItem icon={<BookOpenIcon />}>First</ListItem>
-      <ListItem icon={<KeyIcon />}>Second</ListItem>
-      <ListItem icon={<DesktopIcon />}>Third</ListItem>
-      <List isPlain>
-        <ListItem icon={<DesktopIconClick />}>Try ntest</ListItem>
-      </List>
-    </List>
-*/
-
-export default {
-  title: 'Hello world',
-  component: TagTree
-};
-
-const Template = (args) => <TagTree {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true
-};
+export default TagTree;
