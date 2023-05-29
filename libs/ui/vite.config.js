76b38ef5253979bf6da4883da0eac7fe955894c9
Jason Boxman <jasonb@edseek.com>
2023-05-29 13:37:04 -0400

    Upgrade packages

--- libs/ui/vite.config.js
+++ libs/ui/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
-    },
-})
+  },
+});
